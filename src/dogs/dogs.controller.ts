55e08bfe80bfa361d78be2f45570b8a43e5a1da0
LauraG <marialgullo@gmail.com>
2023-01-23 15:45:14 -0300

    controller fixed

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
-import { Delete } from '@nestjs/common/decorators';
 import { DogsService } from './dogs.service';
 
 @Controller('dogs')
@@ -7,12 +6,12 @@ export class DogsController {
   
   constructor(private dogsService : DogsService){}
 
-  @Get('/:id')
+  @Get(':id')
   getOneDogs(@Param ('id')id:number) {
     return this.dogsService.getOneDog(id);
   }
 
-  @Get('all')
+  @Get()
   getDogs() {
     return this.dogsService.getDogs();
   }
@@ -21,9 +20,5 @@ export class DogsController {
   createDog(@Body() dog){
     return this.dogsService.createDog(dog);
   }
-
-  @Delete()
-  deletedDog(@Param ("id")id:number){
-    return 
-  }
+  
 }
